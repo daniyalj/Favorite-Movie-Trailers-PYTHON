17571c00b89277397a1ffc4255fee0aea0ad610d
daniyalj <daniyaljavedk@gmail.com>
2016-08-14 17:26:16 -0400

    comments

--- media.py
+++ media.py
@@ -2,11 +2,16 @@ import webbrowser
 
 class Movie():
     """This class provides a way to store my favourite movies"""
+
     def __init__(self,movie_title, movie_storyline, poster_image, trailer_youtube):
+        """This function stores movie information
+        Args and parameters: movie title, storyline, poster and trailer"""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """This functions opens the movie trailers in a youtube link
+        args: self"""
         webbrowser.open(self.trailer_youtube_url)
